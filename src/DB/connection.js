4da0f9971cf91e75657b7af2b0b5ab8482b5eca5
Ahmed Mohameed <119890561+ahmed1mohammd@users.noreply.github.com>
2025-08-16 02:39:13 +0300

    Update connection.js

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -1,18 +1,15 @@
 import mongoose from "mongoose";
-const connectDB = async() =>{
 
-   try {
-    const uri = process.env.MONGO_URI
-     const reslut = await mongoose.connect
-     (uri,{serverSelectionTimeoutMS:30000})
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGO_URI;
+    await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 30000
+    });
+    console.log("DB connected successfully ✅");
+  } catch (error) {
+    console.log("Fail to connect on DB ❌", error);
+  }
+};
 
-
-     
-     
-     console.log("DB connected successfully✅");
-     
-   } catch (error) {
-    console.log(`fail to  connect on DB ❌`,error);
-   }
-}
-export default connectDB
+export default connectDB;
